d29f3b8f4b5f9229d9b40f68f5bb097797791c3d
Jake <jakewmayfield@gmail.com>
2021-11-06 14:33:59 -0500

    khaled changes

---
+++ Plotly Files/app.py
@@ -0,0 +1,61 @@
+# Dependencies
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import pandas as pd
+import pymongo
+
+
+app = Flask(__name__)
+# app.config["MONGO_URI"] = "mongodb+srv://abc:1234@cluster0.7hm4r.mongodb.net/job_resume?retryWrites=true&w=majority"
+# mongo = PyMongo(app)
+# # conn = "mongodb+srv://abc:1234@cluster0.7hm4r.mongodb.net/job_resume?retryWrites=true&w=majority"
+# client = PyMongo.MongoClient(conn)
+# db = client.job_resume
+
+conn = "mongodb+srv://abc:1234@cluster0.7hm4r.mongodb.net/job_resume?retryWrites=true&w=majority"
+client = pymongo.MongoClient(conn)
+db = client.job_resume
+d = db.cos_sim.find({})
+a = [x for x in d]
+job_resume = a[0]
+
+def git_top10(name):
+    d = job_resume
+    df = pd.DataFrame([d[name]]).T
+    df.columns = ["value"]
+    return df.sort_values("value", ascending=False).head(10).to_dict()
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+   # Find one record of data from the mongo database
+    # cos_sim = mongo.db.cos_sim.find_one()
+
+
+    
+    # Return template and data
+    return render_template("index.html", options=["Joel", "Mason"])
+
+@app.route("/data/<name1>")
+def get_table(name1):
+   # Find one record of data from the mongo database
+    # cos_sim = mongo.db.cos_sim.find_one()
+
+
+    
+    # Return template and data
+    return render_template("index2.html", options=["Joel", "Mason"], data=git_top10(name1))
+
+
+
+# @app.route("/scrape")
+# def scrape():
+  
+#     resume_dict = mongo.db.resume
+    
+#     # # Update the Mongo database using update and upsert=True
+#     # resume_dict.update({}, mars_data, upsert=True)
+#     return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
